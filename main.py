5de9e10752d08ec0c6328d3fdadf43b3288bf040
Andy Le <4ndyle@gmail.com>
2025-07-07 22:43:43 -0700

    Add google gemini flash-2.0 model

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import os 
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+prompt_answer = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+
 def main():
     print("Hello from agent-41!")
+    print(prompt_answer.text)
+    print(f"Prompt tokens: {prompt_answer.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {prompt_answer.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
