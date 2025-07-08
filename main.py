1aaa8d3e39128c499ebabf34dee725f0e30c13ab
Andy Le <4ndyle@gmail.com>
2025-07-07 22:55:04 -0700

    Update hard coded prompt to use a user provided prompt

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import os 
+import sys
 from dotenv import load_dotenv
 from google import genai
+
+# check if prompt is provided 
+prompt = ""
+if len(sys.argv) > 1:
+    prompt = sys.argv[1]
+else:
+    print("Erorr, prompt cannot be empty. Please provide a prompt after the script")
+    sys.exit(1)
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-prompt_answer = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+prompt_answer = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
 
 def main():
     print("Hello from agent-41!")
