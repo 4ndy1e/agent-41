a871c97242fabb7462951f4a05eb3ac96216334d
Andy Le <4ndyle@gmail.com>
2025-07-13 17:38:51 -0700

    Create function to get file info

---
+++ fucntions/get_files_info.py
@@ -0,0 +1,37 @@
+import os
+
+def get_files_info(working_directory, directory=None): 
+    full_path = os.path.join(working_directory, directory)
+    working_directory_abs = os.path.abspath(working_directory)
+    full_path_abs = os.path.abspath(full_path)
+    
+    # check if directory is outside the working_directory
+    if not full_path_abs.startswith(working_directory_abs):
+        return f"Error: Cannot list '{directory}' as it is outside the permitted working directory"
+    
+    # check if directory is a valid directory
+    if not os.path.isdir(directory):
+        return f"Error: '{directory}' is not a directory"
+    
+    directory_content_names = os.listdir(full_path)
+
+    contents = "Results for current directory:\n"
+    
+    for name in directory_content_names:
+        # get current file path and information
+        try: 
+            file_path = os.path.join(os.path.join(full_path, name))
+            
+            file_size = os.path.getsize(file_path)
+            is_dir = os.path.isdir(file_path)
+            
+            contents += f" - {name}: file_size={file_size}, is_dir: {is_dir}\n"
+        except Exception as error:
+            return f"Error: {error}"
+            
+        
+    return contents
+    
+    
+
+    
