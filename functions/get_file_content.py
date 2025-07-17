4fca14825006dba11118bf5dc1349ca8714a2574
Andy Le <4ndyle@gmail.com>
2025-07-17 00:04:16 -0700

    add function to read file contents

---
+++ functions/get_file_content.py
@@ -0,0 +1,15 @@
+import os
+from ..config import word_limit
+
+def get_file_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
+    working_directory_abs_path = os.path.abspath(working_directory)
+    full_abs_path = os.path.abspath(full_abs_path)
+    
+    if not full_abs_path.startswith(working_directory_abs_path):
+        return f"Error: Cannot read '{file_path} as it is outside the permitted working directory"
+    
+    if not os.path.isfile(full_abs_path):
+        return f"Error: File not found or is not a regular file: '{file_path}'"
+    
+    
