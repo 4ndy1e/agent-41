34d1bcb6c620526912bed82a11354e2e31a1b86f
Andy Le <4ndyle@gmail.com>
2025-07-20 01:04:15 -0700

    Create function to write to file with validation

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,7 +1,25 @@
-from path_validation import validate_path
+import os 
 
 def write_file(working_directory, file_path, content):
-    validationMessage = validate_path(working_directory, file_path)
+    full_path = os.path.join(working_directory, file_path)
+    working_directory_abs_path = os.path.abspath(working_directory)
+    full_abs_path = os.path.abspath(full_path)    
     
-    if validationMessage:
-        return validationMessage
+    if not full_abs_path.startswith(working_directory_abs_path):
+        return f"Error: Cannot write to '{file_path} as it is outside the permitted working directory"
+    
+    try:         
+        directory_name = os.path.dirname(full_abs_path)
+        
+        # create file_path if it does not exist 
+        if not os.path.exists(directory_name):
+            os.makedirs(directory_name)
+            
+        # overwrite into the file 
+        with open(full_abs_path, "w") as f:
+            f.write(content)
+            
+        return f"Successfully wrote to '{file_path}' ({len(content)} characters written)"
+        
+    except Exception as e:
+        return f"Error: {e}"
